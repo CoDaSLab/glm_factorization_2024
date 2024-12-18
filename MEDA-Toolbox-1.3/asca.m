bb2e589571669d93b408f928bba5fc6653bf7b59
mdarmstr <mdsorar@proton.me>
2024-12-18 13:20:08 +0100

    adding correct version of MEDA toolbox

---
+++ MEDA-Toolbox-1.3/asca.m
@@ -0,0 +1,169 @@
+function ascao = asca(paranovao)
+
+% ASCA is a data analysis algorithm for designed experiments. It does a 
+% principal component analysis on the level averages of each experimental 
+% factor in a designed experiment with balanced data. Interactions between 
+% two factors can also be calculated. The original paper for this software 
+% is Zwanenburg, G, Hoefsloot, HCJ, Westerhuis, JA, Jansen, JJ, Smilde, AK.
+% ANOVA–principal component analysis and ANOVA–simultaneous component 
+% analysis: a comparison. Journal of Chemometrics, 2018, 25:561-567.
+%
+% Related routines: parglm, paranova, apca, gasca, create_design
+%
+% ascao = asca(parglm)   % complete call
+%
+%
+% INPUTS:
+%
+% parglm (structure): structure with the factor and interaction matrices, 
+% p-values and explained variance. Obtained with parallel anova (deprecated) 
+% or parallel general linear model.
+%
+%
+% OUTPUTS:
+%
+% ascao (structure): structure that contains scores, loadings, singular
+% values and projections of the factors and interactions.
+%
+%
+% EXAMPLE OF USE (copy and paste the code in the command line)
+%   Random data, two factors, with 4 and 3 levels, but only the first one 
+%   is significative, and 4 replicates:
+%
+% reps = 4;
+% vars = 400;
+% levels = {[1,2,3,4],[1,2,3]};
+%
+% F = create_design(levels,reps);
+%
+% X = zeros(size(F,1),vars);
+% for i = 1:length(levels{1}),
+%     X(find(F(:,1) == levels{1}(i)),:) = simuleMV(length(find(F(:,1) == levels{1}(i))),vars,8) + repmat(randn(1,vars),length(find(F(:,1) == levels{1}(i))),1);
+% end
+%
+% [table, parglmo] = parglm(X, F);
+% table
+% 
+% ascao = asca(parglmo);
+%
+% for i=1:2, % Note, the second factor is shown for the sake of illustration
+%   scores(ascao.factors{i},[],[],sprintf('Factor %d',i),[],ascao.design(:,i));
+% end
+%
+%
+% EXAMPLE OF USE (copy and paste the code in the command line)
+%   Random data, two significative factors, with 4 and 3 levels, and 4 replicates:
+%
+% reps = 4;
+% vars = 400;
+% levels = {[1,2,3,4],[1,2,3]};
+%
+% F = create_design(levels,reps);
+%
+% X = zeros(size(F,1),vars);
+% for i = 1:length(levels{1}),
+%     fi{i} = randn(1,vars);
+% end
+% for j = 1:length(levels{2}),
+%     fj{j} = randn(1,vars);
+% end
+% for i = 1:length(levels{1}),
+%     for j = 1:length(levels{2}),
+%         X(find(F(:,1) == levels{1}(i) & F(:,2) == levels{2}(j)),:) = simuleMV(reps,vars,8) + repmat(fi{i} + fj{j},reps,1);
+%     end
+% end
+%
+% [table, parglmo] = parglm(X, F, [1 2]);
+% table
+% 
+% ascao = asca(parglmo);
+%
+% for i=1:2,
+%   scores(ascao.factors{i},[],[],sprintf('Factor %d',i),[],ascao.design(:,i));
+% end
+%
+%
+% EXAMPLE OF USE (copy and paste the code in the command line)
+%   Random data, two factors with 4 and 3 levels, and 4 replicates, with 
+%   significant interaction:
+%
+% reps = 4;
+% vars = 400;
+% levels = {[1,2,3,4],[1,2,3]};
+%
+% F = create_design(levels,reps);
+%
+% X = zeros(size(F,1),vars);
+% for i = 1:length(levels{1}),
+%     for j = 1:length(levels{2}),
+%         X(find(F(:,1) == levels{1}(i) & F(:,2) == levels{2}(j)),:) = simuleMV(reps,vars,8) + repmat(randn(1,vars),reps,1);
+%     end
+% end
+%
+% [table, parglmo] = parglm(X, F, [1 2]);
+% table
+% 
+% ascao = asca(parglmo);
+%
+% M = ascao.factors{1}.matrix + ascao.factors{2}.matrix + ascao.interactions{1}.matrix;
+% code_levels = F(:,1)*10+F(:,2);
+% scores_pca(M,1:2,X,0,101,[],code_levels);
+% legend(num2str(unique(code_levels)))
+%
+%
+% coded by: José Camacho (josecamacho@ugr.es)
+% last modification: 17/Oct/22
+%
+% Copyright (C) 2022  University of Granada, Granada
+% Copyright (C) 2022  Jose Camacho Paez
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%% Arguments checking
+
+% Set default values
+routine=dbstack;
+assert (nargin >= 1, 'Error in the number of arguments. Type ''help %s'' for more info.', routine(1).name);
+
+
+%% Main code
+
+ascao = paranovao;
+
+%Do PCA on level averages for each factor
+for factor = 1 : ascao.n_factors
+    
+    xf = ascao.factors{factor}.matrix;
+    p = pca_pp(xf,1:rank(xf));
+    
+    ascao.factors{factor}.var = trace(xf'*xf);
+    ascao.factors{factor}.lvs = 1:size(p,2);
+    ascao.factors{factor}.loads = p;
+    ascao.factors{factor}.scores = xf*p;
+    ascao.factors{factor}.scoresV = (xf+ascao.residuals)*p;
+end
+
+%Do PCA on interactions
+for interaction = 1 : ascao.n_interactions
+    
+    xf = ascao.interactions{interaction}.matrix;
+    p = pca_pp(xf,1:rank(xf));
+    
+    ascao.interactions{interaction}.var = trace(xf'*xf);
+    ascao.interactions{interaction}.lvs = 1:size(p,2);
+    ascao.interactions{interaction}.loads = p;
+    ascao.interactions{interaction}.scores = xf*p;
+    ascao.interactions{interaction}.scoresV = (xf+ascao.residuals)*p;
+end
+
---
+++ MEDA-Toolbox-1.3/parglmMC.m
@@ -0,0 +1,352 @@
+function [T, parglmo] = parglmMC(X, F, interactions, prep, n_perm, ts, ordinal, mtc)
+
+% Parallel General Linear Model to obtain factor and interaction matrices 
+% in a crossed experimental design and permutation test for univariate 
+% statistical significance through multiple-test correction methods that 
+% allows variable selection
+%
+% Related routines: parglm, parglmVS, asca, apca, create_design
+%
+% T = parglmMC(X, F)   % minimum call
+% [T, parglmoMC] = parglmMC(X, F, interactions, prep, n_perm, ts, ordinal, mtc)   % complete call
+%
+%
+% INPUTS:
+%
+% X: [NxM] billinear data set for model fitting, where each row is a
+% measurement, each column a variable
+%
+% F: [NxF] design matrix, where columns correspond to factors and rows to
+% levels.
+%
+% interactions: [Ix2] matrix where rows contain the factors for which
+% interactions are to be calculated.
+%
+% prep: [1x1] preprocesing:
+%       1: mean preping
+%       2: autoscaling (default)
+%
+% n_perm: [1x1] number of permutations (1000 by default)
+%
+% ts: [1x1] Use SSQ (0) or the F-value (otherwise, by default) as test statistic  
+%
+% ordinal: [1xF] whether factors are nominal or ordinal
+%       0: nominal
+%       1: ordinal
+%
+% mtc: [1x1] Multiple test correction
+%       1: Bonferroni 
+%       2: Holm step-up or Hochberg step-down
+%       3: Benjamini-Hochberg step-down (FDR, by default)
+%
+% OUTPUTS:
+%
+% T (table): ANOVA-like output table
+%
+% parglmoMC (structure): structure with the factor and interaction
+% matrices, p-values and explained variance 
+%
+%
+% EXAMPLE OF USE (copy and paste the code in the command line)
+% Random data, one factor and two levels, three variables with information 
+% on the factor. This example takes long to compute, you  may reduce the 
+% number of variables or permutations.
+%
+% n_obs = 40;
+% n_vars = 400;
+%
+% class = (randn(n_obs,1)>0)+1;
+% X = simuleMV(n_obs,n_vars,8);
+% X(class==2,1:3) = X(class==2,1:3) + 10;
+%
+% S = rng; % Use same seed for random generators to improve comparability of results
+% [T, parglmo] = parglm(X,class); % No variables selection 
+% rng(S);
+% [TVS, parglmoVS] = parglmVS(X, class); % With multivariate variable selection
+% rng(S);
+% [TMC, parglmoMC] = parglmMC(X, class); % With variable selection through multiple testing correction
+%
+% h = figure; hold on
+% plot([1 n_vars],[parglmo.p parglmo.p],'b-.')
+% plot(parglmoVS.p(parglmoVS.ord_factors),'g-o')
+% plot(parglmoMC.p(parglmoMC.ord_factors),'k-')
+% plot([0,size(X,2)],[0.05 0.05],'r:')
+% plot([0,size(X,2)],[0.01 0.01],'r--')
+% legend('ASCA','VASCA','BH-FDR','alpha=0.05','alpha=0.01','Location','southeast')
+% a=get(h,'CurrentAxes');
+% set(a,'FontSize',14)
+% ylabel('p-values','FontSize',18)
+% xlabel('Variables in selected order','FontSize',18)
+%
+%
+% coded by: José Camacho (josecamacho@ugr.es)
+% last modification: 18/Oct/22
+%
+% Copyright (C) 2022  José Camacho, Universidad de Granada
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+%% Arguments checking
+
+% Set default values
+routine=dbstack;
+assert (nargin >= 2, 'Error in the number of arguments. Type ''help %s'' for more info.', routine(1).name);
+N = size(X, 1);
+M = size(X, 2);
+if nargin < 3 || isempty(interactions), interactions = []; end;
+if nargin < 4 || isempty(prep), prep = 2; end;
+if nargin < 5 || isempty(n_perm), n_perm = 1000; end;
+if nargin < 6 || isempty(ts), ts = 1; end;
+if nargin < 7 || isempty(ordinal), ordinal = zeros(1,size(F,2)); end;
+if nargin < 8 || isempty(mtc), mtc = 3; end;
+
+% Validate dimensions of input data
+assert (isequal(size(prep), [1 1]), 'Dimension Error: 4th argument must be 1-by-1. Type ''help %s'' for more info.', routine(1).name);
+assert (isequal(size(n_perm), [1 1]), 'Dimension Error: 5th argument must be 1-by-1. Type ''help %s'' for more info.', routine(1).name);
+assert (isequal(size(ts), [1 1]), 'Dimension Error: 6th argument must be 1-by-1. Type ''help %s'' for more info.', routine(1).name);
+assert (isequal(size(mtc), [1 1]), 'Dimension Error: 8th argument must be 1-by-1. Type ''help %s'' for more info.', routine(1).name);
+
+
+%% Main code
+
+n_interactions      = size(interactions,1);              % number of interactions
+n_factors           = size(F,2);                         % number of factors
+mtcc                = n_factors + n_interactions;        % correction for the number of tests
+ts_factors         = zeros(n_perm*M+1,n_factors,M);       % sum of squares for factors
+ts_interactions    = zeros(n_perm*M+1,n_interactions,M);  % sum of squares for interactions
+p_factor            = zeros(n_factors,M);                % p-values factors
+p_interaction       = zeros(n_interactions,M);           % p-values interactions
+
+% In column space
+parglmo.factors                = cell(n_factors,1);
+parglmo.interactions           = cell(n_interactions,1);
+
+% preprocess the data
+[Xs,m,dt] = preprocess2D(X,prep);
+X = X./(ones(size(X,1),1)*dt);
+
+SSQ_X = sum(X.^2);
+
+% Make structure with unchanging 'variables'
+parglmo.data           = X;
+parglmo.prep           = prep;
+parglmo.design         = F;
+parglmo.n_factors      = n_factors;
+parglmo.n_interactions = n_interactions;
+
+% Create Design Matrix
+n = 1;
+D = ones(size(X,1),1);
+
+for f = 1 : n_factors
+    if ordinal(f)
+        D(:,n+1) = preprocess2D(F(:,f),1);
+        parglmo.factors{f}.Dvars = n+1;
+        n = n + 1;
+    else
+        uF = unique(F(:,f));
+        parglmo.n_levels(f) = length(uF);
+        for i = 1:length(uF)-1
+            D(find(F(:,f)==uF(i)),n+i) = 1;
+        end
+        parglmo.factors{f}.Dvars = n+(1:length(uF)-1);
+        D(find(F(:,f)==uF(end)),parglmo.factors{f}.Dvars) = -1;
+        n = n + length(uF) - 1;
+    end
+end
+
+for i = 1 : n_interactions
+    for j = parglmo.factors{interactions(i,1)}.Dvars
+        for k = parglmo.factors{interactions(i,2)}.Dvars
+            D(:,end+1) = D(:,j).* D(:,k);
+        end
+    end
+    parglmo.interactions{i}.Dvars = n+1:size(D,2);
+    n = size(D,2);
+end
+  
+% Degrees of freedom
+Tdf = size(X,1);      
+Rdf = Tdf-1;
+for f = 1 : n_factors
+    if ordinal(f)
+        df(f) = 1;
+    else
+        df(f) = size(unique(D(:,parglmo.factors{f}.Dvars),'Rows'),1) -1;
+    end
+    Rdf = Rdf-df(f);
+end
+df_int = [];
+for i = 1 : n_interactions
+    df_int(i) = (df(interactions(i,1)))*(df(interactions(i,2)));
+    Rdf = Rdf-df_int(i);
+end
+if Rdf < 0
+    disp('Warning: degrees of freedom exhausted');
+    return
+end
+    
+% GLM model calibration with LS, only fixed factors
+pD =  pinv(D'*D)*D';
+B = pD*X;
+X_residuals = X - D*B;
+parglmo.D = D;
+parglmo.B = B;
+
+% Create Effect Matrices
+parglmo.inter = D(:,1)*B(1,:);
+SSQ_inter = sum(parglmo.inter.^2);
+SSQ_residuals = sum(X_residuals.^2);
+
+for f = 1 : n_factors
+    parglmo.factors{f}.matrix = D(:,parglmo.factors{f}.Dvars)*B(parglmo.factors{f}.Dvars,:);
+    SSQ_factors(f,:) = sum(parglmo.factors{f}.matrix.^2);
+    F_factors(f,:) = (SSQ_factors(f,:)/df(f))./(SSQ_residuals/Rdf);
+end
+
+% Interactions
+for i = 1 : n_interactions
+    parglmo.interactions{i}.matrix = D(:,parglmo.interactions{i}.Dvars)*B(parglmo.interactions{i}.Dvars,:);
+    SSQ_interactions(i,:) = sum(parglmo.interactions{i}.matrix.^2);
+    F_interactions(i,:) = (SSQ_interactions(i,:)/df_int(i))./(SSQ_residuals/Rdf);
+end
+    
+if n_interactions
+    parglmo.effects = 100*([SSQ_inter' SSQ_factors' SSQ_interactions' SSQ_residuals']./(SSQ_X'*ones(1,2+n_factors+n_interactions)));
+else
+    parglmo.effects = 100*([SSQ_inter' SSQ_factors' SSQ_residuals']./(SSQ_X'*ones(1,2+n_factors+n_interactions)));
+end
+parglmo.residuals = X_residuals;
+
+% Permutations
+for j = 1 : (n_perm * M) % Increase the number of permutations to perform MTC
+    
+    perms = randperm(size(X,1)); % permuted data (permute whole data matrix)
+    
+    B = pD*X(perms, :);
+    X_residuals = X(perms, :) - D*B;
+    SSQ_residuals_p = sum(X_residuals.^2);
+    
+    % Factors
+    SSQ_factors_p = [];
+    F_factors_p = [];
+    for f = 1 : n_factors
+        factor_matrix = D(:,parglmo.factors{f}.Dvars)*B(parglmo.factors{f}.Dvars,:);
+        SSQ_factors_p(f,:) = sum(factor_matrix.^2);
+        F_factors_p(f,:) = (SSQ_factors_p(f,:)/df(f))./(SSQ_residuals_p/Rdf);
+    end
+    
+    % Interactions
+    SSQ_interactions_p = [];
+    F_interactions_p = [];
+    for i = 1 : n_interactions
+        interacts_matrix = D(:,parglmo.interactions{i}.Dvars)*B(parglmo.interactions{i}.Dvars,:);
+        SSQ_interactions_p(i,:) = sum(interacts_matrix.^2);
+        F_interactions_p(i,:) = (SSQ_interactions_p(i,:)/df_int(i))./(SSQ_residuals_p/Rdf);
+    end
+   
+    % Select test statistic to order variables
+    if ts
+        ts_factors(1 + j,:,:) = F_factors_p;
+        ts_interactions(1 + j,:,:) = F_interactions_p;
+    else
+        ts_factors(1 + j,:,:) = SSQ_factors_p;
+        ts_interactions(1 + j,:,:) = SSQ_interactions_p;
+    end
+end        
+   
+
+% Calculate univariate p-values and order variables by relevance
+for factor = 1 : n_factors
+    for var = 1 : M
+        p_factor(factor,var) = (size(find(ts_factors(2:end,factor,var) ...
+            >= ts_factors(1,factor,var)) ,1) + 1)/(n_perm*M + 1);
+    end
+    [~,ord_factors(factor,:)] = sort(p_factor(factor,:),'ascend');
+end
+for interaction = 1 : n_interactions
+    for var = 1 : M
+        p_interaction(interaction,var) = (size(find(ts_interactions(2:end,interaction,var) ...
+            >= ts_interactions(1,interaction,var)) ,1) + 1)/(n_perm*M + 1);
+    end
+    [~,ord_interactions(interaction,:)] = sort(p_interaction(interaction,:),'ascend');
+end
+
+parglmo.ord_factors = ord_factors;
+if n_interactions>0
+    parglmo.ord_interactions = ord_interactions;
+end
+
+% Multiple test correction
+switch mtc
+    case 1 % Bonferroni 
+        p_factor = min(1,p_factor * M); 
+        p_interaction = min(Inf,p_interaction * M); 
+        
+    case 2 % Holm/Hochberg
+        for factor = 1 : n_factors
+            for var = 1 : M 
+                p_factor(factor,ord_factors(factor,var)) = min(1,p_factor(factor,ord_factors(factor,var)) * (M-var+1));
+            end
+        end
+        for interaction = 1 : n_interactions
+            for var = 1 : M
+                p_interaction(interaction,ord_interactions(interaction,var)) = min(1,p_interaction(interaction,ord_interaction(interaction,var)) * (M-var+1));
+            end
+        end
+        
+    case 3 % Benjamini & Hochberg
+        for factor = 1 : n_factors
+            for var = 1 : M
+                p_factor(factor,ord_factors(factor,var)) = min(1,p_factor(factor,ord_factors(factor,var)) * M/var);
+            end
+        end
+        for interaction = 1 : n_interactions
+            for var = 1 : M
+                p_interaction(interaction,ord_interactions(interaction,var)) = min(1,p_interaction(interaction,ord_interaction(interaction,var)) * M/var);
+            end
+        end
+end
+   
+% Multiple test correction for several factors/interactions
+p_factor = min(1,p_factor * mtcc); 
+p_interaction = min(Inf,p_interaction * mtcc);
+
+parglmo.p = [p_factor' p_interaction'];
+
+%% ANOVA-like output table
+
+name={'Mean'};
+for factor = 1 : n_factors
+    name{end+1} = sprintf('Factor %d',factor);
+end
+for interaction = 1 : n_interactions
+    name{end+1} = sprintf('Interaction %d',interaction);
+end
+name{end+1} = 'Residuals';
+name{end+1} = 'Total';
+      
+if n_interactions
+    SSQ = sum([SSQ_inter' SSQ_factors' SSQ_interactions' SSQ_residuals' SSQ_X'],1);
+else
+    SSQ = sum([SSQ_inter' SSQ_factors' SSQ_residuals' SSQ_X'],1);
+end
+par = [mean(parglmo.effects) 100];
+DoF = [1 df df_int Rdf Tdf];
+MSQ = SSQ./DoF;
+F = [nan mean(F_factors,2)' mean(F_interactions,2)' nan nan];
+p_value = [nan mean(p_factor,2)' mean(p_interaction,2)' nan nan];
+
+T = table(name', SSQ', par', DoF', MSQ', F', p_value','VariableNames', {'Source','SumSq','AvPercSumSq','df','MeanSq','AvF','AvPvalue'});
+
